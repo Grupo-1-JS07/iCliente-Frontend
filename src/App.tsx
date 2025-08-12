7bb71d3a980d70ced5d048acc8031517f26744c2
Daniel <mocaibre@gmail.com>
2025-08-11 22:45:39 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { UserProvider } from "./contexts/UserContext";
+
+function App() {
+  return (
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
+}
+
+export default App;
