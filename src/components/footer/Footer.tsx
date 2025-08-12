699a8fc81f799d351368b2e70c728648f9527aae
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-08-12 11:35:24 -0300

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,20 @@
-import {
-  FacebookLogo,
-  InstagramLogo,
-  LinkedinLogo,
-} from "@phosphor-icons/react";
+import { GithubLogoIcon } from "@phosphor-icons/react";
 
 function Footer() {
-  let data = new Date().getFullYear();
+  const data = new Date().getFullYear();
 
   return (
     <>
       <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">
-            Blog Pessoal Generation | Copyright: {data}
+            iCliente &copy; {data}
           </p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogo size={48} weight="bold" />
-            <InstagramLogo size={48} weight="bold" />
-            <FacebookLogo size={48} weight="bold" />
+            <a href="https://github.com/Grupo-1-JS07/iCliente-Frontend" target="_blank">
+            <GithubLogoIcon size={48} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
