7e2a9003dedce520cda541125a7efe746c7ce368
Rselless <selles.rafael27@gmail.com>
2025-08-14 09:29:42 -0300

    Correções

--- src/components/produtos/deletarprodutos/DeletarProdutos.tsx
+++ src/components/produtos/deletarprodutos/DeletarProdutos.tsx
@@ -74,9 +74,9 @@ function DeletarProduto() {
 
   return (
     <div className="container w-1/3 mx-auto">
-      <h1 className="text-4xl text-center my-4">Deletar Produto</h1>
+      <h1 className="text-4xl text-center my-4 text-white">Deletar Produto</h1>
 
-      <p className="text-center font-semibold mb-4">
+      <p className="text-center font-semibold mb-4 text-white">
         Você tem certeza de que deseja apagar o produto a seguir?
       </p>
 
@@ -84,7 +84,7 @@ function DeletarProduto() {
         <header className="py-2 px-6 bg-indigo-600 text-white font-bold text-2xl">
           Produto
         </header>
-        <div className="p-4">
+        <div className="p-4  text-white">
           <p className="text-xl h-full">{produto.nome}</p>
           <p>{produto.descricao}</p>
         </div>
