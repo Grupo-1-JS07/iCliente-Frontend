2973606c6833e2637302bf635ce8ceda7fbdf781
Rselless <selles.rafael27@gmail.com>
2025-09-30 14:26:47 -0300

    Deploy Correções

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { env } from "process";
 
 const api = axios.create({
-  baseURL: env.VITE_API_URL,
+  baseURL: process.env.VITE_API_URL,
 });
 
 console.log("api", api);
